ecfd41f41155312ef8c9bc319a4fa5c51644a3df
ussserrr <andrei4.2008@gmail.com>
2019-12-23 00:50:07 +0300

    migrate to QWidgets

--- stm32pio-gui/app.py
+++ stm32pio-gui/app.py
@@ -1,52 +1,46 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# (c) EVILEG https://evileg.com/ru/post/242/
-
-from PyQt5.QtGui import QGuiApplication
-from PyQt5.QtQml import QQmlApplicationEngine
-from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot
-
-
-class Calculator(QObject):
-    def __init__(self):
-        QObject.__init__(self)
-
-    # cигнал передающий сумму
-    # обязательно даём название аргументу через arguments=['sum']
-    # иначе нельзя будет его забрать в QML
-    sumResult = pyqtSignal(int, arguments=['sum'])
-
-    subResult = pyqtSignal(int, arguments=['sub'])
-
-    # слот для суммирования двух чисел
-    @pyqtSlot(int, int)
-    def sum(self, arg1, arg2):
-        # складываем два аргумента и испускаем сигнал
-        self.sumResult.emit(arg1 + arg2)
-
-    # слот для вычитания двух чисел
-    @pyqtSlot(int, int)
-    def sub(self, arg1, arg2):
-        # вычитаем аргументы и испускаем сигнал
-        self.subResult.emit(arg1 - arg2)
+import sys
+from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QListWidget, QListWidgetItem, \
+    QStackedWidget, QLabel
 
 
-if __name__ == "__main__":
-    import sys
+class CentralWidget(QWidget):
+    def __init__(self, parent=None):
+        super(CentralWidget, self).__init__(parent)
+        grid = QGridLayout()
+        self.setLayout(grid)
 
-    # sys.argv += ['--style', 'material']
+        self.projects_list_widget = QListWidget()
+        # first = QListWidgetItem('First', self.projects_list_widget)
+        self.projects_list_widget.addItem('First')
+        self.projects_list_widget.addItem('Second')
+        self.projects_list_widget.addItem('Third')
 
-    # создаём экземпляр приложения
-    app = QGuiApplication(sys.argv)
-    # создаём QML движок
-    engine = QQmlApplicationEngine()
-    # создаём объект калькулятора
-    calculator = Calculator()
-    # и регистрируем его в контексте QML
-    engine.rootContext().setContextProperty("calculator", calculator)
-    # загружаем файл qml в движок
-    engine.load("main.qml")
+        self.project_window = QStackedWidget()
+        self.project_window.addWidget(QLabel('Hello, World!'))
 
-    engine.quit.connect(app.quit)
+        grid.addWidget(self.projects_list_widget, 0, 0)
+        grid.addWidget(self.project_window, 0, 1)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        self.setWindowTitle(QCoreApplication.applicationName())
+        self.central_widget = CentralWidget()
+        self.setCentralWidget(self.central_widget)
+
+
+if __name__ == '__main__':
+    QCoreApplication.setOrganizationName('ussserrr')
+    QCoreApplication.setApplicationName('stm32pio')
+
+    app = QApplication(sys.argv)
+
+    main_window = MainWindow()
+    main_window.show()
+
     sys.exit(app.exec_())
