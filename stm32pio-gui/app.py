0ab13a206ffc8406e7cf1111549d7122dce21a1b
ussserrr <andrei4.2008@gmail.com>
2019-12-25 18:17:10 +0300

    in progress...

--- stm32pio-gui/app.py
+++ stm32pio-gui/app.py
@@ -2,45 +2,67 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QGridLayout, QListWidget, QListWidgetItem, \
-    QStackedWidget, QLabel
+
+from PyQt5.QtCore import QCoreApplication, QUrl, QAbstractItemModel, pyqtProperty, QAbstractListModel, QModelIndex, \
+    QObject, QVariant, Qt
+from PyQt5.QtGui import QGuiApplication
+from PyQt5.QtQml import qmlRegisterType, QQmlEngine, QQmlComponent, QQmlApplicationEngine
+from PyQt5.QtQuick import QQuickView
+
+import stm32pio.lib
 
 
-class CentralWidget(QWidget):
-    def __init__(self, parent=None):
-        super(CentralWidget, self).__init__(parent)
-        grid = QGridLayout()
-        self.setLayout(grid)
+class ProjectListItem(QObject):
+    def __init__(self, project: stm32pio.lib.Stm32pio, parent=None):
+        super().__init__(parent)
+        self.project = project
 
-        self.projects_list_widget = QListWidget()
-        # first = QListWidgetItem('First', self.projects_list_widget)
-        self.projects_list_widget.addItem('First')
-        self.projects_list_widget.addItem('Second')
-        self.projects_list_widget.addItem('Third')
+    @pyqtProperty('QString')
+    def name(self):
+        return self.project.path.name
 
-        self.project_window = QStackedWidget()
-        self.project_window.addWidget(QLabel('Hello, World!'))
-
-        grid.addWidget(self.projects_list_widget, 0, 0)
-        grid.addWidget(self.project_window, 0, 1)
+    @pyqtProperty('QString')
+    def state(self):
+        return str(self.project.state)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self, parent=None):
-        super(MainWindow, self).__init__(parent)
-        self.setWindowTitle(QCoreApplication.applicationName())
-        self.central_widget = CentralWidget()
-        self.setCentralWidget(self.central_widget)
+class ProjectsList(QAbstractListModel):
+    def __init__(self, projects: list, parent=None):
+        super().__init__(parent)
+        self.projects = projects
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self.projects)
+
+    def data(self, index: QModelIndex, role=None):
+        # print(index, role)
+        if index.row() < 0 or index.row() >= len(self.projects):
+            return QVariant()
+        else:
+            if role == Qt.DisplayRole:
+                return self.projects[index.row()]
+            else:
+                return QVariant()
+
+    def addProject(self, project):
+        self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount())
+        self.projects.append(project)
+        self.endInsertRows()
+
 
 
 if __name__ == '__main__':
-    QCoreApplication.setOrganizationName('ussserrr')
-    QCoreApplication.setApplicationName('stm32pio')
+    app = QGuiApplication(sys.argv)
 
-    app = QApplication(sys.argv)
+    projects = ProjectsList([])
+    projects.addProject(ProjectListItem(stm32pio.lib.Stm32pio('../stm32pio-test-project', save_on_destruction=False)))
+    projects.addProject(ProjectListItem(stm32pio.lib.Stm32pio('../stm32pio-test-project', save_on_destruction=False)))
+    projects.addProject(ProjectListItem(stm32pio.lib.Stm32pio('../stm32pio-test-project', save_on_destruction=False)))
 
-    main_window = MainWindow()
-    main_window.show()
+    view = QQuickView()
+    view.setResizeMode(QQuickView.SizeRootObjectToView)
+    view.rootContext().setContextProperty('projectsModel', projects)
+    view.setSource(QUrl('main.qml'))
+    # view.show()
 
     sys.exit(app.exec_())
